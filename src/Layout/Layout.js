a33fd6933d1154e8d088b83c51df224902262db7
Javier Galarza <jegj57@gmail.com>
2020-06-01 20:15:56 -0500

    Remove extra div and missing css class

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -7,19 +7,13 @@ import SideBar from './SideBar';
 import './Layout.css';
 
 const Layout = props => {
-
 	return (
 		<div className="container">
 			<Header />
 			<div className="content">
 				<SideBar />
 				<main className="main">
-					<div className="overview">
-						<h1 className="overview__heading">
-							Content
-						</h1>
-						{props.children}
-					</div>
+					{props.children}
 				</main>
 			</div>
 		</div>
